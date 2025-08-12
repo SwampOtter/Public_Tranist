5eef12f60e984c1835cd9a4afa781221eb47fa86
swampotter <gino.loverde@gmail.com>
2025-08-12 01:25:21 -0500

    updates 8/12/25

--- master_cleanup.sql
+++ master_cleanup.sql
@@ -6,27 +6,25 @@ WHERE "HQ City" IS NOT NULL
 
 VACUUM FULL ANALYZE public_transit_master;
 
-DO
-$$
-    DECLARE
-        r   record;
-        sql text;
-    BEGIN
-        FOR r IN
-            SELECT column_name
-            FROM information_schema.columns
-            WHERE table_schema = 'public'
-              AND table_name = 'public_transit_master'
-              AND data_type IN ('text', 'character varying')
-            LOOP
-                sql := format(
-                        'SELECT ''%s'' AS column_name, COUNT(*) AS bad_rows
-                         FROM public_transit_master
-                         WHERE %I !~ ''^\d+(\.\d+)?$'' AND %I IS NOT NULL;',
-                        r.column_name, r.column_name, r.column_name
-                       );
-                RAISE NOTICE '%', sql;
-                EXECUTE sql;
-            END LOOP;
-    END
-$$;
+WITH text_cols AS (SELECT column_name
+                   FROM information_schema.columns
+                   WHERE table_schema = 'public'
+                     AND table_name = 'public_transit_master'
+                     AND data_type IN ('text', 'character varying', 'character')),
+     flat AS (SELECT j.key   AS column_name,
+                     j.value AS val
+              FROM public.public_transit_master t
+                       CROSS JOIN LATERAL jsonb_each_text(to_jsonb(t)) AS j(key, value)
+                       JOIN text_cols tc ON tc.column_name = j.key
+              WHERE j.value IS NOT NULL)
+SELECT column_name,
+       COUNT(*) FILTER (
+           WHERE val !~ '^\s*[-+]?\d+(\.\d+)?\s*$'
+           )    AS bad_rows,
+       COUNT(*) AS total_checked
+FROM flat
+GROUP BY column_name
+HAVING COUNT(*) FILTER (
+    WHERE val !~ '^\s*[-+]?\d+(\.\d+)?\s*$'
+    ) > 0
+ORDER BY bad_rows DESC;
